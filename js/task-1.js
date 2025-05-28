5636f56a404f367eb0a7e6d659377051a507fa19
mykola-bakosh <mykolabakos@gmail.com>
2025-05-28 21:51:44 +0300

    8 dan!

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-`use strict`;
-const categories = document.querySelectorAll("#categories .item");
-console.log("Number of categories:", categories.length);
-categories.forEach(item => {
-    console.log("🚀 ~ Category:", item.querySelector("h2").textContent);
-    console.log("Elements:", item.querySelectorAll("ul li").length);
-});
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-`use strict`;
-const input = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-input.addEventListener("input", (event) => {
-  const value = event.currentTarget.value.trim();
-  output.textContent = value === "" ? "Anonymous" : value;
-});
